c80ba8f36289b1cf6e110491ba57f1d91ac5d15a
augusto <augustosaaude@gmail.com>
2024-01-06 22:22:17 -0300

    feat: implemeting deleteUseCase

---
+++ src/Flixer.Catalog.Application/UseCases/Category/DeleteCategory/DeleteCategory.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Flixer.Catalog.Domain.Repository;
+using Flixer.Catalog.Application.Contracts;
+
+namespace Flixer.Catalog.Application.UseCases.Category.DeleteCategory;
+
+public class DeleteCategory : IDeleteCategory
+{
+    private readonly IUnityOfWork _unitOfWork;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public DeleteCategory(ICategoryRepository categoryRepository, IUnityOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteCategoryInput request, CancellationToken cancellationToken)
+    {
+        var category = await _categoryRepository.Get(request.Id, cancellationToken);
+
+        await _categoryRepository.Delete(category, cancellationToken);
+        await _unitOfWork.Commit(cancellationToken);
+
+        return Unit.Value;
+    }
+}
