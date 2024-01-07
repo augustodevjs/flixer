d164c3470a460f8536b9b39f0d0bc335a58b85d8
augusto <augustosaaude@gmail.com>
2024-01-06 23:06:24 -0300

    feat: add useCase of update

---
+++ src/Flixer.Catalog.Application/UseCases/Category/UpdateCategory/IUpdateCategory.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Flixer.Catalog.Application.UseCases.Category.Common;
+
+namespace Flixer.Catalog.Application.UseCases.Category.UpdateCategory;
+
+public interface IUpdateCategory : IRequestHandler<UpdateCategoryInput, CategoryModelOutput>
+{
+}
---
+++ src/Flixer.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInput.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Flixer.Catalog.Application.UseCases.Category.Common;
+
+namespace Flixer.Catalog.Application.UseCases.Category.UpdateCategory;
+
+public class UpdateCategoryInput : IRequest<CategoryModelOutput>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public bool? IsActive { get; set; }
+    public string? Description { get; set; }
+
+    public UpdateCategoryInput(
+        Guid id, 
+        string name, 
+        bool? isActive = null, 
+        string? description = null
+    )
+    {
+        Id = id;
+        Name = name;
+        IsActive = isActive;
+        Description = description;
+    }
+}
