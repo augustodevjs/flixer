c3275915e6bdaeda95d80b1878201034dfe5bb0d
augusto <augustosaaude@gmail.com>
2024-01-14 13:16:50 -0300

    feat: fix teste integration repositoryCategory

--- src/Flixer.Catalog.Infra.Data.EF/UnityOfWork.cs
+++ src/Flixer.Catalog.Infra.Data.EF/UnityOfWork.cs
@@ -11,12 +11,8 @@ public class UnityOfWork : IUnityOfWork
         => _context = context;
 
     public Task Commit(CancellationToken cancellationToken)
-    {
-        return _context.SaveChangesAsync(cancellationToken);
-    }
+        => _context.SaveChangesAsync(cancellationToken);
 
     public Task Rollback(CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
+        => Task.CompletedTask;
 }
--- tests/Flixer.Catalog.IntegrationTests/Infra.Data.EF/UnityOfWork/UnityOfWorkTest.cs
+++ tests/Flixer.Catalog.IntegrationTests/Infra.Data.EF/UnityOfWork/UnityOfWorkTest.cs
@@ -1,48 +1,46 @@
-﻿//using Flixer.Catalog.Infra.Data.EF;
-//using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using InfraData = Flixer.Catalog.Infra.Data.EF;
 
-//namespace Flixer.Catalog.IntegrationTests.Infra.Data.EF.UnityOfWork;
+namespace Flixer.Catalog.IntegrationTests.Infra.Data.EF.UnityOfWork;
 
-//[Collection(nameof(UnitOfWorkTestFixture))]
-//public class UnityOfWorkTest
-//{
-//    private readonly UnitOfWorkTestFixture _fixture;
+[Collection(nameof(UnitOfWorkTestFixture))]
+public class UnityOfWorkTest
+{
+    private readonly UnitOfWorkTestFixture _fixture;
 
-//    public UnityOfWorkTest(UnitOfWorkTestFixture fixture)
-//    {
-//        _fixture = fixture;
-//    }
+    public UnityOfWorkTest(UnitOfWorkTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    [Fact(DisplayName = nameof(Commit))]
+    [Trait("Integration/Infra.Data", "UnitOfWork - Persistence")]
+    public async Task Commit()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var examplecategoriesList = _fixture.GetExampleCategoriesList();
 
-//    [Fact(DisplayName = nameof(Commit))]
-//    [Trait("Integration/Infra.Data", "UnitOfWork - Persistence")]
-//    public async Task Commit()
-//    {
-//        var dbContext = _fixture.CreateDbContext();
-//        var exampleCategory = _fixture.GetExampleCategory();
+        await dbContext.AddRangeAsync(examplecategoriesList);
+        var unitOfWork = new InfraData.UnityOfWork(dbContext);
 
-//        await dbContext.AddAsync(exampleCategory);
+        await unitOfWork.Commit(CancellationToken.None);
 
-//        var unitOfWork = new Catalog.Infra.Data.EF.UnityOfWork(dbContext);
+        var assertDbContext = _fixture.CreateDbContext(true);
 
-//        await unitOfWork.Commit(CancellationToken.None);
-
-//        var assertDbContext = _fixture.CreateDbContext(true);
-//        var savedCategory = await assertDbContext.Categories.FirstOrDefaultAsync(c => c.Id == exampleCategory.Id);
-
-//        savedCategory.Should().NotBeNull();
-//        savedCategory.Should().BeEquivalentTo(exampleCategory);
-//    }
+        var savedCategories = assertDbContext.Categories.AsNoTracking().ToList();
+        savedCategories.Should().HaveCount(examplecategoriesList.Count);
+    }
 
 
-//    [Fact(DisplayName = nameof(Rollback))]
-//    [Trait("Integration/Infra.Data", "UnitOfWork - Persistence")]
-//    public async Task Rollback()
-//    {
-//        var dbContext = _fixture.CreateDbContext();
-//        var unitOfWork = new Catalog.Infra.Data.EF.UnityOfWork(dbContext);
+    [Fact(DisplayName = nameof(Rollback))]
+    [Trait("Integration/Infra.Data", "UnitOfWork - Persistence")]
+    public async Task Rollback()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var unitOfWork = new InfraData.UnityOfWork(dbContext);
 
-//        var task = async () => await unitOfWork.RollBack(CancellationToken.None);
+        var task = async ()
+            => await unitOfWork.Rollback(CancellationToken.None);
 
-//        await task.Should().NotThrowAsync();
-//    }
-//}
+        await task.Should().NotThrowAsync();
+    }
+}
