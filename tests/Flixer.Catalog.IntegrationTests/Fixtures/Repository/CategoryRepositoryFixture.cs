5392f74ec090488dad2aea256c98cd3cc2475ba2
augusto <augustosaaude@gmail.com>
2024-08-28 22:40:47 -0300

    feat: add integration tests to getbyid and create using category entity

---
+++ tests/Flixer.Catalog.IntegrationTests/Fixtures/Repository/CategoryRepositoryFixture.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using Flixer.Catalog.UnitTest.Fixture.Domain.Category;
+
+namespace Flixer.Catalog.IntegrationTests.Fixtures.Repository;
+
+[CollectionDefinition(nameof(CategoryRepositoryFixture))]
+public class CategoryRepositoryFixtureCollection : ICollectionFixture<CategoryRepositoryFixture>
+{
+    
+}
+
+public class CategoryRepositoryFixture : BaseFixture
+{
+    public CategoryFixture CategoryFixture { get; } = new();
+    
+    
+}
---
+++ tests/Flixer.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+using FluentAssertions;
+using Flixer.Catalog.IntegrationTests.Fixtures.Repository;
+
+namespace Flixer.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
+
+[Collection(nameof(CategoryRepositoryFixture))]
+public class CategoryRepositoryTest
+{
+    private readonly CategoryRepositoryFixture _fixture;
+
+    public CategoryRepositoryTest(CategoryRepositoryFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+    public async Task CategoryRepository_CreateCategory()
+    {
+        var dbContext = _fixture.CreateDbContext("integration-tests-repository");
+        var exampleCategory = _fixture.CategoryFixture.GetValidCategory();
+        var categoryRepository = new Catalog.Infra.Data.EF.Repositories.CategoryRepository(dbContext);
+
+        categoryRepository.Create(exampleCategory);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var dbCategory = await dbContext.Categories.FindAsync(exampleCategory.Id);
+        
+        dbCategory.Should().NotBeNull();
+        dbCategory?.Name.Should().Be(exampleCategory.Name);
+        dbCategory?.IsActive.Should().Be(exampleCategory.IsActive);
+        dbCategory?.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+        dbCategory?.Description.Should().Be(exampleCategory.Description);
+    }
+    
+    [Fact]
+    [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+    public async Task CategoryRepository_GetById()
+    {
+        var dbContext = _fixture.CreateDbContext("integration-tests-repository");
+        var exampleCategory = _fixture.CategoryFixture.GetValidCategory();
+
+        await dbContext.Categories.AddAsync(exampleCategory);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+        
+        var categoryRepository = new Catalog.Infra.Data.EF.Repositories.CategoryRepository(dbContext);
+
+        var dbCategory = await categoryRepository.GetById(exampleCategory.Id);
+        
+        dbCategory.Should().NotBeNull();
+        dbCategory?.Id.Should().Be(exampleCategory.Id);
+        dbCategory?.Name.Should().Be(exampleCategory.Name);
+        dbCategory?.IsActive.Should().Be(exampleCategory.IsActive);
+        dbCategory?.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+        dbCategory?.Description.Should().Be(exampleCategory.Description);
+    }
+}
